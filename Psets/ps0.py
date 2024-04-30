2a031b7a6f53a08bc86768f2fb278a7e85c1c5a5
Anil Bhusal <anil.bhusal15@gmail.com>
2024-04-30 12:29:50 +0545

    Pset0

---
+++ Psets/ps0.py
@@ -0,0 +1,21 @@
+# Problem Set 0
+# Name: Anil Bhusal
+# Collaborators: None
+# Time Spent: 5 Minutes
+
+import numpy
+
+def main():
+    # Assigning values to Variables 'x' and 'y'
+    x = 5
+    y = 8
+
+    # Adding two variables 'x' and 'y' and store in variable 'z'
+    z = x + y
+
+    #
+    a = numpy.log2(z)
+
+
+if __name__ == "__main__":
+    main()
