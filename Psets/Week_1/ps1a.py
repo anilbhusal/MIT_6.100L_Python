5ed5c0d8a95d9cd156822096f6c353eaeff81d84
Anil Bhusal <anil.bhusal15@gmail.com>
2024-04-30 19:55:04 +0545

    ps1

---
+++ Psets/Week_1/ps1a.py
@@ -0,0 +1,20 @@
+## 6.100A PSet 1: Part A
+## Name: Anil Bhusal
+## Time Spent:
+## Collaborators:
+
+##################################################################################
+## Get user input for yearly_salary, portion_saved and cost_of_dream_home below ##
+##################################################################################
+yearly_salary = float(input("Enter yearly Salary: "))
+portion_saved = float(input("Portion of the salary saved: "))
+cost_of_dream_home = float(input("Cost of dream home: "))
+
+#########################################################################
+## Initialize other variables you need (if any) for your program below ##
+#########################################################################
+
+
+###############################################################################################
+## Determine how many months it would take to get the down payment for your dream home below ## 
+###############################################################################################
